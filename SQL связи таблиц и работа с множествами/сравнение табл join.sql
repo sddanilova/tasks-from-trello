7c6a50ba1a93c93afdba526ed48e3e3d2fdddae7
Sonya <dssd-2000@mail.ru>
2022-02-14 21:29:45 +0300

    добвлена новая версия задачи сравнения табл

--- SQL связи таблиц и работа с множествами/сравнение табл join.sql
+++ SQL связи таблиц и работа с множествами/сравнение табл join.sql
@@ -7,7 +7,16 @@ natural left join
 where ca<>cb
 order by a,b
 
-
+--решение просто с left join, более правильное
+SELECT AB.a, AB.b, AA.ca, BB.cb From
+(select a,b from t1 union select a,b from t2) AB
+left join
+(select a, b, count(*) ca from t1 group by a,b) AA
+on AB.a=AA.a
+left join (select a, b, count(*) cb from t2 group by a,b) BB
+on AB.a=BB.a
+where not ca=cb or BB.a is null or AA.a is null
+order by AB.a, AB.b
 
 
 
